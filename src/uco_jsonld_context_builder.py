6aa1898ac4723cc549f60fada758d19ea43a23e9
Kevin Fairbanks <1482470+kfairbanks@users.noreply.github.com>
2022-08-09 12:54:47 -0400

    Removing testing logic from main context builder script

--- src/uco_jsonld_context_builder.py
+++ src/uco_jsonld_context_builder.py
@@ -76,7 +76,7 @@ class ContextBuilder:
             self.context_str = self.context_str[:-1]
         self.context_str += "\n\t}\n}"
 
-    def get_ttl_files(self, subdirs=[]) -> list:
+    def get_ttl_files(self, subdirs=[]) -> typing.List[pathlib.Path]:
         """
         Finds all turtle (.ttl) files in directory structure
         @subdirs - Optional list used to restrict search to particular
@@ -99,8 +99,6 @@ class ContextBuilder:
         # Path.glob returns dot files, unlike shell's glob.
         # The uco.ttl file is also skipped because the Python output removes
         # supplementary prefix statements.
-        ontology_filepaths : typing.List[pathlib.Path] = []
-
         file_list = []
         _logger.debug(top_srcdir)
 
@@ -158,7 +156,8 @@ class ContextBuilder:
         # Taking the ':' off the end of the key
         k = t_split[1][:-1]
         v = t_split[2]
-        v=v.strip()[1:-1]
+        # Taking the angle brackets of the IRIs
+        v = v.strip()[1:-1]
         if k in iri_dict.keys():
             # _logger.debug(f"'{k}' already exists")
             if iri_dict[k] != v:
@@ -217,7 +216,7 @@ class ContextBuilder:
             else:
                 self.datatype_properties_dict[root] = [dtp_obj]
         return
-    
+
     def process_DatatypeProperties(self):
         for ttl_file in self.ttl_file_list:
             self.__process_DatatypePropertiesHelper(in_file=ttl_file)
@@ -252,18 +251,17 @@ class ContextBuilder:
                     if int(sh_triple2[2]) <= 1:
                         if op_obj.shacl_count_lte_1 is not None:
                             _logger.debug(f"\t\t\t**MaxCount Double Definition? {triple[0].n3(graph.namespace_manager)}")
-                            # print("\t\t**MaxCount Double Definition?")
                         op_obj.shacl_count_lte_1 = True
                     else:
                         _logger.debug(f"\t\t\t***Large max_count: {sh_triple2[2]}")
-                
+
             if root in self.object_properties_dict.keys():
                 _logger.debug(f"None Unique Entry Found:\t {ns_prefix}:{root}")
                 self.object_properties_dict[root].append(op_obj)
             else:
                 self.object_properties_dict[root] = [op_obj]
         return
-    
+
     def process_ObjectProperties(self):
         for ttl_file in self.ttl_file_list:
             self.__process_ObjectPropertiesHelper(in_file=ttl_file)
@@ -281,8 +279,8 @@ class ContextBuilder:
         for ttl_file in ttl_file_list:
             with open(ttl_file, 'r') as file:
                 for line in file:
-                    if re.search("^\@prefix", line):
-                        # _logger.debug(line.strip())
+                    if re.search("^@prefix", line):
+                        _logger.debug(f"Prefix: {ttl_file}\t{line.strip()}")
                         self.__add_to_iri_dict(in_prefix=line.strip())
 
     def print_minimal_datatype_properties(self) -> str:
@@ -397,60 +395,6 @@ def main():
         out_f.close()
     else:
         print(cb.context_str)
-    #return
-
-    # Testing
-    graph = rdflib.Graph()
-    graph.parse("../tests/uco_monolithic.ttl", format="turtle")
-    graph.serialize("_uco_monolithic.json-ld", format="json-ld")
-    graph2 = rdflib.Graph()
-    import json
-    tmp_c = json.loads(cb.context_str)
-    graph2.parse("_uco_monolithic.json-ld", format="json-ld", context_data=tmp_c)
-    # graph2.parse("../tests/uco_monolithic.ttl", format="turtle", context_data=tmp_c)
-    # graph2.parse("../tests/uco_monolithic.ttl", format="turtle")
-    # graph.serialize("__uco_monolithic.json-ld", context_data=tmp_c, format="json-ld", auto_compact=False)
-    # graph2.serialize("__uco_monolithic.json-ld", context_data=tmp_c, format="json-ld", auto_compact=True)
-    graph2.serialize("__uco_monolithic.json-ld", context_data=tmp_c, format="json-ld", auto_compact=True)
-    # graph2.serialize("__uco_monolithic.json-ld", format="json-ld", auto_compact=True)
-    return
-    # for triple in graph.triples((None,None,rdflib.OWL.Class)):
-    #    # print(triple[0].fragment)
-    #     print(triple[0].n3(graph.namespace_manager))
-    #     print(f"\t{triple}")
-    
-    return
-    graph = rdflib.Graph()
-    graph.parse("../tests/uco_monolithic.ttl", format="turtle")
-    "Make sure to do an itter that looks for rdflib.OWL.class"
-    limit = 10000
-    count = 0
-    # for triple in graph.triples((None,None,rdflib.OWL.Class)):
-    # for sh_triple in graph.triples(None,"rdflib.term.URIRef('http://www.w3.org/ns/shacl#property')", None):
-    # for sh_triple in graph.triples(None,rdflib.term.URIRef('http://www.w3.org/ns/shacl#property'), None):
-    print("###<SHACL Search>")
-    for sh_triple in graph.triples((None, rdflib.term.URIRef('http://www.w3.org/ns/shacl#property'), None)):
-        print(f"**sh_triple:{sh_triple}")
-    print("###</SHACL Search>")
-
-    for triple in graph.triples((None ,None, None)):
-        # print(triple[0].fragment)
-        # print(triple[0].n3(graph.namespace_manager))
-        print(triple)
-        sh_prop_node = None
-        for sh_triple in graph.triples((triple[0], rdflib.term.URIRef('http://www.w3.org/ns/shacl#property'), None)):
-            print(f"\t**sh_triple:{sh_triple[2].n3(graph.namespace_manager)}")
-            sh_prop_node = sh_triple[2]
-            for triple3 in graph.triples((sh_prop_node, rdflib.term.URIRef('http://www.w3.org/ns/shacl#maxCount'), None)):
-                print(f"\t***sh_prop_triple:{triple3}")
-                print(f"\t***sh_prop_triple:{triple3[2]}")
-
-        # for t in list(triple):
-        #     print(f"{t.n3(graph.namespace_manager)}")
-        # print(triple)
-        count += 1
-        if count >= limit:
-            sys.exit()
     return
 
     # TODO: context keyword in graph parse and graph serialize
--- tests/context_builder/context_tester.py
+++ tests/context_builder/context_tester.py
@@ -4,26 +4,34 @@ import json
 import rdflib
 import sys
 import subprocess
+import os
 
+# Test graph file in JSON format
 test_file = "action_result_NO_CONTEXT.json"
-output_file = "temp_cntxt.json"
+# File to which context will be written
+output_file = "_temp_cntxt.json"
+# Serialization of graph without using context
+no_cntxt_out = "_test_out_no_cntxt.json-ld"
+# Serialization of graph using context
+cntxt_out = "_test_out_cntxt.json-ld"
+
 # Execute Context builder
 cmd = "python ../../src/uco_jsonld_context_builder.py --output " + output_file
 print(cmd)
 subprocess.run(cmd.split())
+
 with open(output_file, 'r') as file:
 	tmp_c = json.load(file)
-# print(tmp_c)
+
 graph = rdflib.Graph()
 graph.parse(test_file, format="json-ld")
-graph.serialize("test_out_no_cntxt.json-ld", format="json-ld")
+graph.serialize(no_cntxt_out, format="json-ld")
 graph2 = rdflib.Graph()
 graph2.parse(test_file, format="json-ld", context_data=tmp_c)
-graph.serialize("test_out_cntxt.json-ld", context_data=tmp_c, format="json-ld", auto_compact=True)
-# graph2.parse("../tests/uco_monolithic.ttl", format="turtle", context_data=tmp_c)
-# graph2.parse("../tests/uco_monolithic.ttl", format="turtle")
-# graph.serialize("__uco_monolithic.json-ld", context_data=tmp_c, format="json-ld", auto_compact=False)
-# graph2.serialize("__uco_monolithic.json-ld", context_data=tmp_c, format="json-ld", auto_compact=True)
-#graph2.serialize("__uco_monolithic.json-ld", context_data=tmp_c, format="json-ld", auto_compact=True)
-# graph2.serialize("__uco_monolithic.json-ld", format="json-ld", auto_compact=True)
+graph.serialize(cntxt_out, context_data=tmp_c, format="json-ld", auto_compact=True)
+
+# Clean up
+# os.remove(output_file)
+# os.remove(no_cntxt_out)
+# os.remove(cntxt_out)
 sys.exit()
