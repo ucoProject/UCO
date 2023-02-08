4a0cc022111e226eef518e981079a5d2bdc92c89
b0bkaT <tbobka.mitre@gmail.com>
2023-02-08 14:20:31 -0500

    Updated create-catalog script to work with python 3.8

--- src/create-catalog-v001.xml.py
+++ src/create-catalog-v001.xml.py
@@ -1,11 +1,18 @@
 import xml.etree.ElementTree as ETree
 import os
+from xml.dom import minidom
 
 
-XML_VERSION_INFO = b'<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>'
+# XML version string to being file
+XML_VERSION_INFO = '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>'
 
 
 def get_list_of_ttl_files(ont_dir):
+    """
+    Returns the list of turtle files that needs to be made into an import statement
+    :param ont_dir: The directory that contains the uco ontology files
+    :return: A dictionary of turtle files per directory to convert into import statements
+    """
     ttl_files = {}
     for (root, dir, file) in os.walk(ont_dir):
         for name in file:
@@ -17,11 +24,19 @@ def get_list_of_ttl_files(ont_dir):
 
 
 def create_catalog_xml(ttl_files):
+    """
+    Writes the catalog-v001.xml file to use for local importing
+    :param ttl_files: The dictionary containing turtle files to convert into import statements
+    """
     xml_root = ETree.Element('catalog')
+    # Creates the proper attributes for the root node
     xml_root.attrib = {'prefer': 'public', 'xmlns': 'urn:oasis:names:tc:entity:xmlns:xml:catalog'}
 
-    for key, val in ttl_files.items():
-        if key != 'master':
+    # Sorts turtle files to ensure imports are alphabetical
+    sorted_ttl_files = sorted(ttl_files.items())
+    # Creates each import statement as a child node
+    for key, val in sorted_ttl_files:
+        if key != 'master': # Skips master (uco.ttl) import
             uri_string = os.path.join('..', key, val)
             name_string = f'https://ontology.unifiedcyberontology.org/uco/{key}'
             uri = ETree.SubElement(xml_root, 'uri')
@@ -31,13 +46,15 @@ def create_catalog_xml(ttl_files):
                 'name': name_string
             }
 
+    # Writes the xml tree to the specified output file
     output_file = os.path.abspath(os.path.join('ontology', 'uco', 'master', 'catalog-v002.xml'))
-    with open(output_file, 'wb') as output:
-        output.write(XML_VERSION_INFO)
-        output.write(b'\n')
-        ETree.indent(xml_root, level=0)  # Only available in python 3.9 or later
-        output.write(ETree.tostring(xml_root, encoding='utf-8', method='xml'))
-        output.write(b'\n')
+    xml_tree_string = minidom.parseString(ETree.tostring(xml_root, encoding='utf-8', 
+                                     method='xml').decode('utf-8')).toprettyxml(indent='  ')
+    with open(output_file, 'w') as output:
+        output.write(f'{XML_VERSION_INFO}\n')
+        # ETree.indent(xml_root, level=0)  -- Only available in python 3.9 or later
+        # Writes the xml tree without the default 'version' statement
+        output.write(f'{xml_tree_string[23:]}\n')                
 
 
 if __name__ == "__main__":
